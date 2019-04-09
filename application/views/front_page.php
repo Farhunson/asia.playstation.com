55fb22e4b9462335252120976cff8ff529ecad56
Muhammad Naimullah <muh.naimullah2359@gmail.com>
2019-04-09 19:16:52 +0800

    first
    front_page.php

---
+++ application/views/front_page.php
@@ -0,0 +1,304 @@
+<!doctype HTML>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  
+<style>
+* {
+  box-sizing: border-box;
+}
+ 
+body {
+  margin: 0;
+}
+ 
+.navbar {
+  overflow: hidden;
+  background-color: #1F1F1F;
+  font-family: Arial, Helvetica, sans-serif;
+  height: 65px;
+  padding-left: 10px;
+  border-radius: 0px;
+  
+}
+ 
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+ 
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+ 
+.dropdown .dropbtn {
+  font-size: 14px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font: inherit;
+  margin: 0;
+  padding-top: 27px;
+  font-family: 'ＭＳ Ｐゴシック';
+  font-weight: 400;
+}
+ 
+.navbar .dropdown:hover .dropbtn {
+  background-color: #003791;
+  opacity :1;
+  transform: translateY(0);
+}
+ 
+.dropdown-content {
+  display: none;
+  position: absolute;
+  width: 100%;
+  left: 0;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+}
+ 
+.dropdown-content .header {
+  padding: 16px;
+  color: white;
+  margin-top: 10px;
+}
+ 
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 10px;
+  background-color: #ccc;
+  height: 250px;
+}
+ 
+.column a {
+  float: none;
+  color: black;
+  padding: 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+ 
+.column a:hover {
+  background-color: #ddd;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+ 
+#grad1 {
+  background: linear-gradient(to bottom, transparent 100%, black 50%);
+  background: rgb(55,127,170);
+  background: radial-gradient(circle, rgba(55,127,170,1) 30%, rgba(0,55,145,1) 51%);
+}
+ 
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    height: auto;
+  }
+}
+
+#search {
+    position: absolute;
+    top: 50px;
+    right: 20px;
+    width: 200px;
+    height: 36px;
+    margin: 0;
+    padding: 0 30px 0 10px;
+    color: #ffffff;
+    background-color: #303030;
+    border: 0;
+    border-radius: 3px;
+    outline: 0;
+    font-size: 12px;
+    font-family: "SST W55 Regular", "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", "メイリオ", Meiryo, Osaka, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
+    box-sizing: border-box;
+    opacity: 0.2;
+}
+
+#search_id {
+    margin: 0;
+    float: right;
+    padding-right: 25px;
+    padding-top: 20px;
+}
+</style>
+<title>PS4 | Games | PlayStation</title>
+</head>
+<body>
+   <nav class="navbar navbar-expand-md" style="background-color: black; height: 40px;padding: 0;margin: 0" >
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ml-auto" style="float:right; padding-top:15px" >
+                <li class="nav-item">
+                    <img src = "<?php echo base_url('img/sony_logo.svg'); ?>"  />
+                </li>
+            </ul>
+        </div>
+    </nav>
+<div class="navbar" style="margin:0" >
+  <a href="https://asia.playstation.com/en-id/"><img src = "<?php echo base_url('img/icon_ps_pc.svg'); ?>"></a>
+  <div class="dropdown">
+    <button class="dropbtn">PRODUCTS
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content" id="grad1">
+      <div class="header" >
+        <h2>PRODUCTS</h2>
+      </div>  
+    </div>
+  </div>
+  <div class="dropdown">
+    <button class="dropbtn">GAMES
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content" id="grad1">
+      <div class="header">
+        <h2>GAMES</h2>
+      </div>  
+    </div>
+  </div>
+  <div class="dropdown">
+    <button class="dropbtn">PSN
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content" id="grad1">
+      <div class="header">
+        <h2>PSN</h2>
+      </div>  
+    </div>
+  </div>
+    <div class="dropdown">
+    <button class="dropbtn">NEWS
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content" id="grad1">
+      <div class="header">
+        <h2>PSN</h2>
+      </div>  
+    </div>
+  </div>
+    <div class="dropdown">
+    <button class="dropbtn">SUPPORT
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content" id="grad1">
+      <div class="header">
+        <h2>PSN</h2>
+      </div>  
+    </div>
+  </div>
+    <div class="dropdown">
+    <button class="dropbtn">BUY
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content" id="grad1">
+      <div class="header">
+        <h2>PSN</h2>
+      </div>  
+    </div>
+  </div>
+  <div class="dropdown">
+    <button class="dropbtn">SIGN IN
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content" id="grad1">
+      <div class="header">
+        <h2>PSN</h2>
+      </div>  
+    </div>
+  </div>
+  <div>
+  	<input id="search" type="text" placeholder="Search.." style="float: right" src="img src = "<?php echo base_url('img/menu_navi-searchsvg'); ?>">
+   	<img id="search_id" src="<?php echo base_url('img/menu_navi-search.svg'); ?>" onclick='image(this)'>
+  </div>
+</div>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="<?php echo base_url('img/spidey.png');?>" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="<?php echo base_url('img/kindom.png');?>" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="<?php echo base_url('img/dgone.png');?>" alt="New york" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  <div class="parsys section">
+	<div class="box960">
+	<div class="row">  
+		<div class="layoutRowParsys parsys"><div class="baseComponent caption section">  
+			<h2 class="js-style lg-center xs-font28 "  data-sm-style="border-bottom-style: none;" data-xs-style="border-bottom-style: none;" align="middle">
+             Garansi PlayStation 4 di Indonesia
+			</h2>
+		</div>
+    </div>
+	</div>
+	<div class="row  all-pull-right  ">
+		<div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
+			<div class="layoutRowParsys parsys"><div class="img baseComponent section">
+				<img class="img-responsive js-img-src js-style" src="<?php echo base_url('img/indo.jpg'); ?>" style="width:100%;height:100%;">
+			</div>
+		</div>
+	</div>
+	<div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">  
+        <div class="layoutRowParsys parsys"><div class="baseComponent section text">
+			<div  ><p>&nbsp;</p>
+			<p>Dengan Sistem Garansi baru, para pengguna di Indonesia akan dapat menikmati manfaat berikut:</p>
+			<p>·&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Produk <i>Authentic Official</i> (Asli Resmi) – Produk Resmi PlayStation akan memiliki stiker hologram yang menyatakan “Produk Resmi Indonesia”</p>
+			<p>·&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Lebih dari 50 Pusat Layanan di seluruh Indonesia – Pengguna dapat membawa Produk Resmi PlayStation mereka ke beragam pusat layanan di Indonesia.</p>
+			<p>&nbsp;</p>
+			<button type="button" class="btn btn-primary">LEARN MORE</button>
+			</div>
+		</div>
+	</div>
+   </div>
+  
+
+
+</body>
+</html>
